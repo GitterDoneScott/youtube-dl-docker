0b30956ae7e087c995393b09a19f9fa84c1ee624
John Wood <john@kolon.co.uk>
2018-08-15 01:24:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
 
-./root/update.sh
+# Check if we have get_iplayer
+if [[ ! -f /usr/local/bin/youtube-dl ]]
+then
+  /root/update.sh start
+fi
 
-sleep 9999
- 
+if [[ ! -f /usr/local/bin/youtube-dl ]]
+then  # pause for checking things out...
+  echo err1 - Error occurred, pausing for 9999 seconds for investigation
+  sleep 9999
+fi
+
+# Set some nice defaults
+#if [[ ! -f /root/.get_iplayer/options ]]
+#then
+#  echo No options file found, adding some nice defaults...
+#  /root/get_iplayer --prefs-add --whitespace
+#  /root/get_iplayer --prefs-add --subs-embed
+#  /root/get_iplayer --prefs-add --metadata
+#  /root/get_iplayer --prefs-add --nopurge
+  
+#  echo Removing non-standard download location from existing PVR files...
+  # so downloads don't get saved in the container
+#  sed -i '/^output/d' /root/.get_iplayer/pvr/*
+#fi
+
+# Force output location to a separate docker volume
+#echo Forcing output location...
+#/root/get_iplayer --prefs-add --output="/root/output/"
+
+if [[ -f /usr/local/bin/youtube-dl ]]
+then
+  # Start cron
+  #service cron start
+  
+  # Keep restarting - for when the get_iplayer script is updated
+  while true
+  do
+    /usr/local/bin/youtube-dl-webui -C /root/config/youtube-dl-webui.conf
+  done 
+else
+  echo err2 - Error occurred, pausing for 9999 seconds for investigation
+  sleep 9999 # when testing, keep container up long enough to check stuff out
+fi
